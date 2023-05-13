c76fb2d6a73488124c82a0bced8b546725a7eee0
alice <dothinh.dev@gmail.com>
2023-05-13 14:57:04 +0700

    add score board

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
 from random import randint
 
+#============================ FUNC ============================
+def spaceFill(value, space):
+    value = str(value)
+    valueLen = len(value)
+    result = ''
+    for index in range(space):
+        if index < valueLen:
+            result += value[index]
+        else:
+            result += ' '
+    return result
+
 #============================ MENU ============================
 LOW_NUMBER  = 0
 HIGH_NUMBER = 100
@@ -25,43 +37,64 @@ def printMainMenu(playerName = 'Ẩn danh'):
     print('+----------Menu-----------+')
     for index in range(26):
         if index == 0:
-            print('+',end='')
+            print('|',end='')
         if index == 25:
-            print('+')
+            print('|')
             continue
         if index < nameLen:
             print(playerName[index],end='')
         else:
             print(' ',end='')
-    print('+-------------------------+')
+    print('+----------MENU-----------+')
     print('|Nhập 1 để chơi           |')
     print('|Nhập 2 để xem bảng điểm  |')
     print('|Nhập 0 để thoát          |')
     print('+-------------------------+')
-    userChoice = int(input(f'+Nhâp lựa chọn: '))
+    userChoice = int(input(f'>Nhập lựa chọn: '))
     return userChoice
 
 def printGameOverMenu():
-    print('+----------Menu-----------+')
+    print('+-------GAME CLEAR--------+')
     print('|Nhập 1 để chơi lại       |')
     print('|Nhập 2 để xem bảng điểm  |')
     print('|Nhập 3 để trở lại menu   |')
     print('|Nhập 0 để thoát          |')
     print('+-------------------------+')
-    userChoice = int(input(f'+Nhâp lựa chọn: '))
+    userChoice = int(input(f'>Nhập lựa chọn: '))
+    return userChoice
+
+def printScoreBoard(scores):
+    records = []
+    for index, score in enumerate(scores):
+        record = '|' + spaceFill(index + 1, 3) + '|' + spaceFill(score[0], 6) + '|' + spaceFill(score[1], 14) + '|'
+        records.append(record)
+        
+    print('+---+----SCORE BOARD------+')
+    print('|   |Đáp án|Số lần đoán   |')
+    for record in records:
+        print(record)
+    print('+---+---------------------+')
+    print('+-------------------------+')
+    print('|Nhập 3 để trở lại menu   |')
+    print('|Nhập 0 để thoát          |')
+    print('+-------------------------+')
+    userChoice = int(input(f'>Nhập lựa chọn: '))
     return userChoice
 
 #============================ MAIN ============================
 if __name__ == '__main__':
     os.system('cls')
-    scoreBoard = []
+    scores = []
     cursor = printMainMenu()
     os.system('cls')
     while cursor != 0:
         if cursor == 1:
             thisNumber, thisAttempt = playGame()
-            scoreBoard.append([thisNumber, thisAttempt])
+            scores.append([thisNumber, thisAttempt])
             cursor = printGameOverMenu()
+            os.system('cls')
+        if cursor == 2:
+            cursor = printScoreBoard(scores)
             os.system('cls')
         if cursor == 3:
             cursor = printMainMenu()
